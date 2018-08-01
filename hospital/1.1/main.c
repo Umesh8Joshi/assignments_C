66965120da5bf24ccdd8a4d98ee01ff5d2b6c7d9
umesh <Umesh14el007@gmail.com>
2018-08-01 16:20:28 +0530

    Final program ready to use

--- hospital/1.1/main.c
+++ hospital/1.1/main.c
@@ -1,10 +1,17 @@
+/* Write a menu driven program to insert data of patient which will be stored in a linked list and at the termination of program it will save that data into a txt file */
 
-// headers included
+/********************************************************************
+ * @file    main.c                                                  *
+ * @author  Umesh                                                   *
+ * @brief   To enter data using linked list and writing into a file *
+ * @date    01/08/2018                                              *
+ ********************************************************************/
+
+// header files
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-// Function declarations
+#include <ctype.h>
 
 // structure for linked list
 struct Patient{
@@ -21,14 +28,28 @@ struct node{
     struct node *next;
 };
 
-// head and current node
 
+// head and current node
 struct node *head = NULL;
 struct node *curr = NULL;
 
-// Insert data
-// By using some of the bizzar methods
+// Function declarations
+void insertFirst(struct Patient);
+struct Patient getData(void);
+void printList(void);
+void printGiven(struct node*);
+int editChoiceOpt(void);
+struct node* edit(void);
+struct node* delete(void);
+void fileSave(void);
+void menu(void);
 
+/**Function     insertFirst
+ * @date        01/08/2018
+ * @brief       Insert data at head
+ * @param       struct Patient
+ * @return      void
+ */
 void insertFirst(struct Patient p){
     struct node *link = (struct node*)malloc(sizeof(struct node));
     
@@ -44,6 +65,12 @@ void insertFirst(struct Patient p){
     head = link;
 }
 
+/**Function     getData
+ * @date        01/08/2018
+ * @brief       get Data from keyboard
+ * @param       struct Patient
+ * @return      void
+ */
 struct Patient getData(void){
     struct Patient p;
     printf("\nEnter Name of Patient\t");
@@ -61,7 +88,13 @@ struct Patient getData(void){
     return p;
 }
 
-void printList(){
+/**Function     printlist
+ * @date        01/08/2018
+ * @brief       Print all data from linked list
+ * @param       void
+ * @return      void
+ */
+void printList(void){
     struct node *ptr = head;
     printf("\nPrinting all data\n");
     while(ptr != NULL){
@@ -77,6 +110,12 @@ void printList(){
     }
 }
 
+/**Function     printGiven
+ * @date        01/08/2018
+ * @brief       Print particular item from linked list
+ * @param       struct node pointer
+ * @return      void
+ */
 void printGiven(struct node* n){
     printf("$$$$$$$$$$$$$$$$$$$$\n");
     printf("Name :\t %s\n", n->patient.name);
@@ -88,6 +127,12 @@ void printGiven(struct node* n){
     printf("Symptoms :\t %s", n->patient.symp);
 }
 
+/**Function     editChoice
+ * @date        01/08/2018
+ * @brief       get choice for editing data
+ * @param       struct Patient
+ * @return      int : choice entered by user
+ */
 int editChoiceOpt(){
     int choice = 0;
     printf("Enter choice\n1.\tTo edit Name\n2.\tTo edit Age\n3.\tTo edit Address\n4.\tTo edit Symptoms\n5.\tBack to MAIN\n");
@@ -95,6 +140,13 @@ int editChoiceOpt(){
     return choice;
 }
 
+/**Function     edit()
+ * @date        01/08/2018
+ * @brief       edit particular item from list
+ * @param       void
+ * @return      struct nod pointer
+ *                  : NULL if not found
+ */
 struct node* edit(){
     struct node *curr = head;
     char nameToFind[20];
@@ -105,7 +157,6 @@ struct node* edit(){
         return NULL;
     }
 
-    // while(curr->patient.name != nameToFind){
     while(strcmp(curr->patient.name, nameToFind) != 0){
         if(curr->next == NULL){
             printf("No data found");
@@ -146,6 +197,13 @@ struct node* edit(){
     return curr;
 }
 
+/**Function     delete
+ * @date        01/08/2018
+ * @brief       delete particular item from linked list
+ * @param       void
+ * @return      struct node pointer
+ *                  : NULL if not found
+ */
 struct node* delete(){
     struct node* curr = head;
     struct node* prev = NULL;
@@ -177,53 +235,77 @@ struct node* delete(){
     return curr;
 }
 
-void fileSave(){
+/**Function     fileSave
+ * @date        01/08/2018
+ * @brief       write to a file
+ * @param       void
+ * @return      void
+ */
+void fileSave(void){
     FILE *fp;
     fp = fopen("hospital.txt", "w");
     struct node* curr = head;
-    while(curr != NULL){
-        fprintf(fp, "-----------------------------------");
-        fprintf(fp, "\nName :\t %s", curr->patient.name);
-        fprintf(fp, "\nAge :\t %d", curr->patient.Age);
-        fprintf(fp, "\n\t\tAddress");
-        fprintf(fp, "\nHouse Name:\t%s", curr->patient.houseName);
-        fprintf(fp, "\nStreet    :\t %s", curr->patient.street);
-        fprintf(fp, "\nCity      :\t %s", curr->patient.city);
-        fprintf(fp, "\nSymptoms  :\t %s", curr->patient.symp);
-        fprintf(fp, "\n");
-        curr = curr->next;
+    if(curr == NULL){
+        fclose(fp);
+    }else{
+        while(curr != NULL){
+            fprintf(fp, "-----------------------------------");
+            fprintf(fp, "\nName :\t %s", curr->patient.name);
+            fprintf(fp, "\nAge :\t %d", curr->patient.Age);
+            fprintf(fp, "\n\t\tAddress");
+            fprintf(fp, "\nHouse Name:\t%s", curr->patient.houseName);
+            fprintf(fp, "\nStreet    :\t %s", curr->patient.street);
+            fprintf(fp, "\nCity      :\t %s", curr->patient.city);
+            fprintf(fp, "\nSymptoms  :\t %s", curr->patient.symp);
+            fprintf(fp, "\n");
+            curr = curr->next;
+        }
+        fclose(fp);
     }
 }
 
-void menu(){
+/**Function     menu
+ * @date        01/08/2018
+ * @brief       main menu for choices
+ * @param       void
+ * @return      void
+ */
+void menu(void){
     int choice = 0;
     printf("\nWelcome to Hospital managment System\nPlease select your option");
     printf("1.\tInsert Data\n2.\tDelete Data\n3.\tEdit Data\n4.\tPrint All\n5.\tExit\n");
-    scanf("%d", &choice);
+    char c = getchar();
+    if(isdigit(c)){
+        choice = (int)c;
+    }else{
+        printf("%c", c);
+        printf("\n\n\t\tWrong Choice\n\n");
+        menu();
+    }
     switch(choice){
-        case 1:
+        case '1':
             insertFirst(getData());
             menu();
             break;
-        case 2:
+        case '2':
             delete();
             menu();
             break;
-        case 3:
+        case '3':
             edit();
             menu();
             break;
-        case 4:
+        case '4':
             printList();
             menu();
             break;
-        case 5:
+        case '5':
             fileSave();
             break;
     }
 }
 
-int main(int argc, char const *argv[])
+int main()
 {
     menu();
     return 0;
